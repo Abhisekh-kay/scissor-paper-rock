c3736f8412920905c6fecf876b81f6d30516572a
Abhisekh <abhisekhkayasth@gmail.com>
2022-09-05 22:44:02 +0545

    Stuck on playing the playround function 5 times

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+//Computer chooses from "ROCK, PAPER, SCISSORS"
 function getComputerChoice(){
    let result= Math.floor(Math.random()*3)+1;
     if (result==1){
@@ -11,34 +12,46 @@ function getComputerChoice(){
     }
 }
 
-
-function playRound(playerSelection,computerSelection){
-   if (playerSelection==computerSelection){
-    return "Draw";
+//PLAYS ONE ROUND OF GAME
+function playRound(playerSelection,computerSelection)
+{
+   if (playerSelection==computerSelection)
+   {
+    console.log("Draw") ;
    }
-   if (playerSelection=="scissors" && computerSelection=="paper"){
-    return "You win! Scissors beats paper";
+   if (playerSelection=="scissors" && computerSelection=="paper")
+   {
+    console.log("You win! Scissors beats paper") ;
    }
-    else if (playerSelection=="scissors" && computerSelection=="rock"){
-    return "You lose! Rock beats scissors";
+    else if (playerSelection=="scissors" && computerSelection=="rock")
+    {
+    console.log("You lose! Rock beats scissors");
    }
-   else if (playerSelection=="paper" && computerSelection=="scissors"){
-    return "You lose! scissors beats paper";
+   else if (playerSelection=="paper" && computerSelection=="scissors")
+   {
+    console.log("You lose! scissors beats paper") ;
    }
-   else if (playerSelection=="paper" && computerSelection=="rock"){
-    return "You win! Paper beats rock";
+   else if (playerSelection=="paper" && computerSelection=="rock")
+   {
+    console.log("You win! Paper beats rock") ;
    }
-   else if (playerSelection=="rock" && computerSelection=="scissors"){
-    return "You win! Rock beats scissors";
+   else if (playerSelection=="rock" && computerSelection=="scissors")
+   {
+    console.log("You win! Rock beats scissors") ;
    }
-   else if (playerSelection=="rock" && computerSelection=="paper"){
-    return "You lose! Paper beats rock";
+   else if (playerSelection=="rock" && computerSelection=="paper")
+   {
+    console.log("You lose! Paper beats rock") ;
    }
 }
+
+//PLAYS 5 ROUNDS OF GAME
+function game(){}
+
+
 
 const playerSelection = prompt("Enter your choice").toLowerCase();
 const computerSelection= getComputerChoice();
 console.log(playerSelection)
 console.log(computerSelection);
-
 console.log(playRound(playerSelection, computerSelection));
