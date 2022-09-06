76992b4ff698b482d8c46af8fe4d416f91630a79
Abhisekh <abhisekhkayasth@gmail.com>
2022-09-06 22:38:04 +0545

    Made function that plays 5 round of games

--- app.js
+++ app.js
@@ -1,15 +1,15 @@
 //Computer chooses from "ROCK, PAPER, SCISSORS"
 function getComputerChoice(){
    let result= Math.floor(Math.random()*3)+1;
-    if (result==1){
-        return "scissors";
-    }
-    else if(result==2){
-        return "paper";
-    }
-    else{
-        return "rock";
-    }
+     if (result==1){
+         return "scissors";
+     }
+     else if(result==2){
+         return "paper";
+     }
+     else{
+         return "rock";
+     }
 }
 
 //PLAYS ONE ROUND OF GAME
@@ -17,41 +17,65 @@ function playRound(playerSelection,computerSelection)
 {
    if (playerSelection==computerSelection)
    {
-    console.log("Draw") ;
+    console.log("Draw")
    }
    if (playerSelection=="scissors" && computerSelection=="paper")
    {
     console.log("You win! Scissors beats paper") ;
+    UserScore++;
    }
     else if (playerSelection=="scissors" && computerSelection=="rock")
     {
     console.log("You lose! Rock beats scissors");
+    ComputerScore++;
    }
    else if (playerSelection=="paper" && computerSelection=="scissors")
    {
     console.log("You lose! scissors beats paper") ;
+    ComputerScore++;
    }
    else if (playerSelection=="paper" && computerSelection=="rock")
    {
     console.log("You win! Paper beats rock") ;
+    UserScore++;
    }
    else if (playerSelection=="rock" && computerSelection=="scissors")
    {
     console.log("You win! Rock beats scissors") ;
+    UserScore++;
    }
    else if (playerSelection=="rock" && computerSelection=="paper")
    {
     console.log("You lose! Paper beats rock") ;
+    ComputerScore++;
    }
 }
 
 //PLAYS 5 ROUNDS OF GAME
-function game(){}
+function game(){
+    for (i=0; i<5; i++){
+
+        const playerSelection = prompt("Enter your choice").toLowerCase();
+        console.log(playerSelection)
+        const computerSelection= getComputerChoice();
+        console.log(computerSelection);
+        playRound(playerSelection, computerSelection);
+    }
+    if (UserScore ==5 || ComputerScore!= 5){
+        console.log("You won");
+        console.log(UserScore);
+    }
+    else if (ComputerScore==5 || UserScore!=5){
+       console.log("Computer won");
+    }
+    else{
+        console.log("No one won");
+    }
+}
 
 
 
-const playerSelection = prompt("Enter your choice").toLowerCase();
-const computerSelection= getComputerChoice();
-console.log(playerSelection)
-console.log(computerSelection);
-console.log(playRound(playerSelection, computerSelection));
+let UserScore =0;
+let ComputerScore =0;
+// console.log(playRound(playerSelection, computerSelection));
+game();
