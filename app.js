ffcf92ccc4739a33260be669d35dbefd087848e6
Abhisekh <abhisekhkayasth@gmail.com>
2022-12-07 11:49:20 +0545

    Final

--- app.js
+++ app.js
@@ -1,82 +1,89 @@
-//Computer chooses from "ROCK, PAPER, SCISSORS"
-function getComputerChoice(){
-   let result= Math.floor(Math.random()*3)+1;
-     if (result==1){
-         return "scissors";
-     }
-     else if(result==2){
-         return "paper";
-     }
-     else{
-         return "rock";
-     }
+//Computer chooses from "SCISSORS, PAPER, ROCK"
+function getComputerChoice() {
+    let choices = ['scissors', 'paper', 'rock']
+    return choices[Math.floor(Math.random() * choices.length)]
 }
+
+let UserScore = 0;
+let ComputerScore = 0;
+const buttons = document.querySelectorAll('input')
+let result = ''
+
 
 //PLAYS ONE ROUND OF GAME
-function playRound(playerSelection,computerSelection)
-{
-   if (playerSelection==computerSelection)
-   {
-    console.log("Draw")
-   }
-   if (playerSelection=="scissors" && computerSelection=="paper")
-   {
-    console.log("You win! Scissors beats paper") ;
-    UserScore++;
-   }
-    else if (playerSelection=="scissors" && computerSelection=="rock")
-    {
-    console.log("You lose! Rock beats scissors");
-    ComputerScore++;
-   }
-   else if (playerSelection=="paper" && computerSelection=="scissors")
-   {
-    console.log("You lose! scissors beats paper") ;
-    ComputerScore++;
-   }
-   else if (playerSelection=="paper" && computerSelection=="rock")
-   {
-    console.log("You win! Paper beats rock") ;
-    UserScore++;
-   }
-   else if (playerSelection=="rock" && computerSelection=="scissors")
-   {
-    console.log("You win! Rock beats scissors") ;
-    UserScore++;
-   }
-   else if (playerSelection=="rock" && computerSelection=="paper")
-   {
-    console.log("You lose! Paper beats rock") ;
-    ComputerScore++;
-   }
+function playRound(playerSelection) {
+    const computerSelection = getComputerChoice()
+
+    if (playerSelection == computerSelection) {
+        resultsContent.textContent = ('It\'s a tie. You both chose \n\n\n' + playerSelection
+            + "\tPlayer score: \n" + UserScore + "\n  Computer score: " + ComputerScore)
+    }
+    if (playerSelection == "scissors" && computerSelection == "paper") {
+        UserScore++;
+        resultsContent.textContent = ('You win! ' + playerSelection + ' beats ' + computerSelection
+            + "\tPlayer score: " + UserScore + "Computer score: " + ComputerScore)
+    }
+    else if (playerSelection == "scissors" && computerSelection == "rock") {
+        ComputerScore++;
+        resultsContent.textContent = ('You lose! ' + computerSelection + ' beats ' + playerSelection
+            + "Player score: " + UserScore + "Computer score: " + ComputerScore)
+    }
+    else if (playerSelection == "paper" && computerSelection == "scissors") {
+        ComputerScore++;
+        resultsContent.textContent = ('You lose! ' + computerSelection + ' beats ' + playerSelection
+            + "Player score: " + UserScore + "Computer score: " + ComputerScore)
+    }
+    else if (playerSelection == "paper" && computerSelection == "rock") {
+        UserScore++;
+        resultsContent.textContent = ('You win! ' + playerSelection + ' beats ' + computerSelection
+            + "Player score: " + UserScore + "Computer score: " + ComputerScore)
+    }
+    else if (playerSelection == "rock" && computerSelection == "scissors") {
+        UserScore++;
+        resultsContent.textContent = ('You win! ' + playerSelection + ' beats ' + computerSelection
+            + "Player score: " + UserScore + "Computer score: " + ComputerScore)
+    }
+    else if (playerSelection == "rock" && computerSelection == "paper") {
+        ComputerScore++;
+        resultsContent.textContent = ('You lose! ' + computerSelection + ' beats ' + playerSelection
+            + "Player score: " + UserScore + "Computer score: " + ComputerScore)
+    }
+    if (UserScore == 5) {
+        resultsContent.textContent = 'You won the game! Reload the page to play again'
+        alert('Your score: ' + UserScore + '\nComputer Score: ' + ComputerScore)
+        disableButtons()
+    }
+
+    else if (ComputerScore == 5) {
+        resultsContent.textContent = 'I won the game! Reload the page to play again'
+        alert('Your score: ' + UserScore + '\nComputer Score: ' + ComputerScore)
+        disableButtons()
+    }
+
 }
 
-//PLAYS 5 ROUNDS OF GAME
-function game(){
-    for (i=0; i<5; i++){
+// Event-listeners to the buttons.
+buttons.forEach(button => {
+    button.addEventListener('click', function () {
+        playRound(button.value)
+    })
+})
 
-        const playerSelection = prompt("Enter your choice").toLowerCase();
-        console.log(playerSelection)
-        const computerSelection= getComputerChoice();
-        console.log(computerSelection);
-        playRound(playerSelection, computerSelection);
-    }
-    if (UserScore > ComputerScore){
-        console.log("You won");
-        console.log(UserScore + " wins for you");
-    }
-    else if (ComputerScore> UserScore){
-       console.log("Computer won");
-       console.log(ComputerScore + " wins for computer");
-    }
-    else{
-        console.log("No one won");
-    }
+// For disabling the buttons to play it more times.
+function disableButtons() {
+    buttons.forEach(elem => {
+        elem.disabled = true;
+    })
 }
 
+// Displaying the status in the DOM.
+const gamestatus = document.querySelector('#status');
+const statusContent = document.createElement('div');
+statusContent.classList.add('statusContent');
+gamestatus.appendChild(statusContent);
 
-
-let UserScore =0;
-let ComputerScore =0;
-// console.log(playRound(playerSelection, computerSelection));
-game();
+// Displaying the reuslts in the DOM.
+const results = document.querySelector('#result');
+const resultsContent = document.createElement('div');
+resultsContent.classList.add('resultsContent');
+results.appendChild(resultsContent);
